5a80117f7c10fa32f75625efd40d7f45aef171a2
kibin <ohkibin@gmail.com>
2023-01-15 23:11:42 +0200

    add track counter

--- util.js
+++ util.js
@@ -1,11 +1,13 @@
 const findSongName = (fileName) => {
     let backSlashIndex = 0;
+
     for (let i = fileName.length - 1; i > 0; i--) {
         if (fileName[i] === `\\`[0]) {
             backSlashIndex = i;
             break;
         };
     }
+
     return fileName.slice(backSlashIndex + 1);
 }
 
@@ -17,4 +19,37 @@ const findHighQualityMp3 = (results, songName) => {
     return results[0];
 }
 
-module.exports = { findSongName, findHighQualityMp3 };
+const TrackCounter = (amount) => {
+    const map = {
+        fail: 0,
+        success: 0,
+        left: amount,
+    }
+    const check = type => {
+        map.left -= 1;
+        map[type] += 1;
+
+        if (map.left <= 0) {
+            console.log('\n')
+            console.log('\n')
+            console.log(`Downloaded: ${map.success}`.green);
+            console.log(`Failed: ${map.fail}`.red);
+            console.log('\n')
+            console.log('All done!'.rainbow);
+            process.exit();
+        } else {
+            console.log(`Tracks left: ${map.left}`.yellow);
+        }
+    }
+
+    return {
+        fail: () => check('fail'),
+        success: () => check('success'),
+    }
+}
+
+module.exports = {
+    findSongName,
+    findHighQualityMp3,
+    TrackCounter,
+};
